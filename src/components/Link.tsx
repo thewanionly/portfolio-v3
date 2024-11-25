3759e2205e80e793bf4eac6fad401dd7a68b8e35
thewanionly <pelloani@gmail.com>
2024-11-26 05:09:17 +0800

    Install tailwind

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,6 @@
 export const Link = ({ label, href }: { label: string; href: string }) => {
   return (
-    <a href={href} target="_blank">
+    <a href={href} target="_blank" className="hover:underline">
       {label}
     </a>
   );
