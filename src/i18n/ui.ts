2d4c8b214a9b21be2b8b4f2e3d81b2fd809808e6
thewanionly <pelloani@gmail.com>
2024-11-30 00:19:37 +0800

    Add i18n (WIP)

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  ja: "Japanese",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "hero.title": "Hello, I'm Elloani Pitogo.",
+    "hero.description": `I'm a Software Engineer based in The Philippines. I focus in Front-End Web
+      Development for over 6 years now.`,
+  },
+  ja: {
+    "hero.title": "こんにちは、エロアニ・ピトゴです。",
+    "hero.description":
+      "私はフィリピンを拠点とするソフトウェア エンジニアです。6 年以上にわたり、フロントエンド Web 開発に注力しています。",
+  },
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,16 @@
+import { ui, defaultLang } from "./ui";
+
+export function getLangFromUrl(url: URL) {
+  // const [, lang] = url.pathname.split("/");
+  const lang = url.searchParams.get("lang") ?? defaultLang;
+
+  if (lang in ui) return lang as keyof typeof ui;
+
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
+}
