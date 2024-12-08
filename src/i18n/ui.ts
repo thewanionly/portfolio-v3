47a3722c538da6f892dfa02d3d238e55afe4426c
thewanionly <pelloani@gmail.com>
2024-12-09 01:01:08 +0800

    Add translations for skills

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,10 +10,14 @@ export const ui = {
     "hero.title": "Hello, I'm Elloani Pitogo.",
     "hero.description": `I'm a Software Engineer based in The Philippines. I focus in Front-End Web
       Development for over 6 years now.`,
+    "hero.skills.title": "My skills includes:",
+    "hero.skills.and.more": "and more...",
   },
   ja: {
     "hero.title": "こんにちは、エルワニ・ピトゴ です。",
     "hero.description":
       "私 は フィリピン を 拠点 と する ソフトウェア エンジニア です。6年 以上 に わたり、フロントエンド Web 開発 に 注力して います。",
+    "hero.skills.title": "私の スキル には 以 が 含まれます:",
+    "hero.skills.and.more": "さらに...",
   },
 } as const;
